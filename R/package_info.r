44bacb447f621a8397e5757cde118edf357482bb
Sebastian Kranz <sebastian.kranz@uni-ulm.de>
2015-03-15 11:07:13 +0100

    Example of an RTutor Problem Set package

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorExample"
+  
+  # Name of problem sets in the package
+  ps = c("Intro","Example")
+  
+  list(package=package, ps = ps)
+}
